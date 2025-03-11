1e6dfa41a50ed53c15c8078cdaff75f685c0cf66
Oluwasegun-rm <segunsojijohn@gmail.com>
2025-03-11 13:44:46 -0400

    text additions to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <>
       <div>
         <div className="Intro">
-          <h2>Oluwasegun Soji-John</h2>
+          <h2>Hey, I am Oluwasegun Soji-John</h2>
           <p>Computer Science Major at Morgan State University.</p>
           <p>
             I'm a software engineering and data science enthusiast. As a
@@ -19,16 +19,37 @@ export default function About() {
             also a tryhard in Call Of Duty Mobile.
           </p>
         </div>
+
+        <div className="About Me">
+          <h2>About Me</h2>
+          <p>
+            I'm a software engineering and data science enthusiast. As a
+            research assistant at the Machine Intelligence and Data Science
+            (MINDS) Lab (Morgan State), I work on backend web development
+            projects, building efficient systems and APIs. I also tutor college
+            students in programming and algorithms. Passionate about solving
+            real-world problems through software and data, I'm always exploring
+            new challenges at the intersection of engineering and analytics.
+          </p>
+          <p>
+            A fun fact about me is that I play table tennis (Ping pong) and I'm
+            also a tryhard in Call Of Duty Mobile.
+          </p>
+        </div>
+
+
         <div className="Experience">
           <h3>Experience</h3>
           <ul>
             <li>
-              <h4 className="role">Research Assistant</h4>
-              <span>
+              <h4 className="role">Undergraduate Researcher</h4>
+              {/* <span>
                 <p className="company">MINDS LAB</p>
                 <p className="date"> -Present</p>
-              </span>
-              <p className="role-description"></p>
+              </span> */}
+              <p className="role-description">
+              I design and develop backend systems, REST APIs, and data pipelines to support research at the MINDS Lab. My work involves optimizing database performance, automating data workflows, and integrating structured datasets into web-based research platforms, improving data accessibility and analysis.
+              </p>
             </li>
             <li>
               <h4 className="role"></h4>
