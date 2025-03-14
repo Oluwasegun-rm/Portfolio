90f9fb96110b52f569da80ac473ee38d264d954c
Oluwasegun-rm <segunsojijohn@gmail.com>
2025-03-14 11:56:56 -0400

    skills page update

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,53 +1,97 @@
 export default function Skills() {
   return (
     <>
-      <div>
+      <div className="skills-container">
         <h2>My Skills</h2>
         <div className="Categories">
-          <p className="category-name">Languages</p>
-          <ul>
-            <li>Python</li>
-            <li>Javascript</li>
-            <li>Typescript</li>
-            <li>R</li>
-          </ul>
-        </div>
-
-
-        <div className="Categories">
-          <p className="category-name">Web Development</p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-          </ul>
-        </div>
-
-        <div className="Categories">
-          <p className="category-name">Frameworks & Libraries</p>
-          <ul className="Items">
-            <li>Next js</li>
-            <li> Pandas</li>
-            <li>React</li>
-            <li>Flask</li>
-            <li>APIs</li>
-            <li>Seaborn</li>
-            <li>Matplotlib</li>
-            <li>MongoDB</li>
-            <li>Tailwind</li>
-            <li>Numpy</li>
+          <h3 className="category-name">Languages</h3>
+          <ul className="items">
+            <span className="items-span">
+              <li>Python</li>
+            </span>
+            <span className="items-span">
+              <li>Javascript</li>
+            </span>
+            <span className="items-span">
+              <li>Typescript</li>
+            </span>
+            <span className="items-span">
+              <li>R</li>
+            </span>
           </ul>
         </div>
 
         <div className="Categories">
-          <p className="category-name">Dev Tools</p>
-          <ul>
-            <li>Git</li>
-            <li>Github</li>
-            <li>Figma</li>
-            <li>Docker</li>
+          <h3 className="category-name">Web Development</h3>
+          <ul className="items">
+            <span className="items-span">
+              <li>HTML</li>
+            </span>
+            <span className="items-span">
+              <li>CSS</li>
+            </span>
           </ul>
         </div>
-        
+
+        <div className="Categories">
+          <h3 className="category-name">Frameworks & Libraries</h3>
+          <ul className="items">
+            <span className="items-span">
+              <li className="skill">Next js</li>
+            </span>
+            <span className="items-span">
+              <li className="skill"> Pandas</li>
+            </span>
+
+            <span className="items-span">
+              <li className="skill">React</li>
+            </span>
+
+            <span className="items-span">
+              <li className="skill">Flask</li>
+            </span>
+
+            <span className="items-span">
+              <li className="skill">APIs</li>
+            </span>
+
+            <span className="items-span">
+              <li className="skill">Seaborn</li>
+            </span>
+
+            <span className="items-span">
+              <li className="skill">Matplotlib</li>
+            </span>
+
+            <span className="items-span">
+              <li className="skill">MongoDB</li>
+            </span>
+            <span className="items-span">
+              <li className="skill">Tailwind</li>
+            </span>
+            <span className="items-span">
+              <li className="skill">Numpy</li>
+            </span>
+          </ul>
+        </div>
+
+        <div className="Categories">
+          <h3 className="category-name">Dev Tools</h3>
+          <ul className="items">
+            <span className="items-span">
+              <li className="skill">Git</li>
+            </span>
+            <span className="items-span">
+              <li className="skill">Github</li>
+            </span>
+            <span className="items-span">
+              <li className="skill">Figma</li>
+            </span>
+            <span className="items-span">
+              <li className="skill">Docker</li>
+            </span>
+          </ul>
+        </div>
       </div>
     </>
   );
